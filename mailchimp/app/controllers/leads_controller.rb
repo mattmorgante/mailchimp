13f73eabbc22194b6156611ec0ee149f5f75e466
Matt <matthewmorgante@gmail.com>
2016-05-02 13:52:21 -0400

    setting up form

---
+++ mailchimp/app/controllers/leads_controller.rb
@@ -0,0 +1,16 @@
+class LeadsController < ApplicationController
+  def create
+    @lead = Lead.new(lead_params)
+    if @lead.save
+      redirect_to root_path, notice: "Saved Successfully!"
+    else 
+      redirect_to root_path, notice: "Failed to save"
+    end 
+  end
+
+  private 
+
+    def lead_params
+      params.require(:lead).permit(:name, :email, :device, :country)
+    end
+end
